88d49b4eb4555e313d367780bec19f23a88469a0
Harshpreet <harshpreet27@ymail.com>
2017-06-28 11:58:33 +0530

    updating 2nd commit

--- self_info.py
+++ self_info.py
@@ -3,6 +3,7 @@ import requests,urllib
 APP_ACCESS_TOKEN = '672364325.e2145dd.524319b348584aec8229a023ca106d37'
 BASE_URL = 'https://api.instagram.com/v1/'
 
+
 def self_info():
   request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
   print 'GET request url : %s' % (request_url)
@@ -20,6 +21,7 @@ def self_info():
     print 'Status code other than 200 received!'
 
 
+
 def get_user_id(insta_username):
     request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
@@ -34,6 +36,8 @@ def get_user_id(insta_username):
         print 'Status code other than 200 received!'
         exit()
 
+
+#Getting other User's info.
 
 def get_user_info(insta_username):
     user_id = get_user_id(insta_username)
@@ -55,6 +59,10 @@ def get_user_info(insta_username):
     else:
         print 'Status code other than 200 received!'
 
+
+
+#Downloading own recent post.
+
 def get_own_post():
     request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
@@ -72,6 +80,7 @@ def get_own_post():
         print 'Status code other than 200 received!'
 
 
+#Downloading user's recent post.
 
 def get_user_post(insta_username):
     user_id = get_user_id(insta_username)
@@ -94,8 +103,66 @@ def get_user_post(insta_username):
         print 'Status code other than 200 received!'
 
 
+
+
+def get_post_id(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            return user_media['data'][0]['id']
+        else:
+            print 'There is no recent post of the user!'
+            exit()
+    else:
+        print 'Status code other than 200 received!'
+        exit()
+
+
+#Liking a user's post.
+
+def like_a_post(insta_username):
+    media_id = get_post_id(insta_username)
+    request_url = (BASE_URL + 'media/%s/likes') % (media_id)
+    payload = {"access_token": APP_ACCESS_TOKEN}
+    print 'POST request url : %s' % (request_url)
+    post_a_like = requests.post(request_url, payload).json()
+    if post_a_like['meta']['code'] == 200:
+        print 'Like was successful!'
+    else:
+        print 'Your like was unsuccessful. Try again!'
+
+
+#Posting a comment.
+
+def post_a_comment(insta_username):
+    media_id = get_post_id(insta_username)
+    comment_text = raw_input("Your comment: ")
+    payload = {"access_token": APP_ACCESS_TOKEN, "text" : comment_text}
+    request_url = (BASE_URL + 'media/%s/comments') % (media_id)
+    print 'POST request url : %s' % (request_url)
+
+    make_comment = requests.post(request_url, payload).json()
+
+    if make_comment['meta']['code'] == 200:
+        print "Successfully added a new comment!"
+    else:
+        print "Unable to add comment. Try again!"
+
+
+
 self_info()
+
 print get_user_id("vivek_shivam")
 get_user_info("vivek_shivam")
 get_own_post()
 get_user_post("vivek_shivam")
+get_post_id("vivek_shivam")
+like_a_post("vivek_shivam")
+post_a_comment("vivek_shivam")
