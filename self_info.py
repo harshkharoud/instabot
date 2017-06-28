06c2e378248d5e7c12c6bb2a4a531d39748ebb9a
Harshpreet <harshpreet27@ymail.com>
2017-06-28 10:57:29 +0530

    2nd commit

--- self_info.py
+++ self_info.py
@@ -1,4 +1,4 @@
-import requests
+import requests,urllib
 
 APP_ACCESS_TOKEN = '672364325.e2145dd.524319b348584aec8229a023ca106d37'
 BASE_URL = 'https://api.instagram.com/v1/'
@@ -55,6 +55,47 @@ def get_user_info(insta_username):
     else:
         print 'Status code other than 200 received!'
 
+def get_own_post():
+    request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    own_media = requests.get(request_url).json()
+
+    if own_media['meta']['code'] == 200:
+        if len(own_media['data']):
+            image_name = own_media['data'][0]['id'] + '.jpeg'
+            image_url = own_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+
+
+def get_user_post(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            image_name = user_media['data'][0]['id'] + '.jpeg'
+            image_url = user_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+
 self_info()
 print get_user_id("vivek_shivam")
 get_user_info("vivek_shivam")
+get_own_post()
+get_user_post("vivek_shivam")
