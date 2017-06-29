e4d79ea2e4f727e80148c6fdd1603b62f9a96d92
Harshpreet <harshpreet27@ymail.com>
2017-06-29 11:26:57 +0530

    3rd Commit

--- self_info.py
+++ self_info.py
@@ -157,12 +157,39 @@ def post_a_comment(insta_username):
 
 
 
-self_info()
+def start_bot():
+    while True:
+        print '\n'
+        print 'Hey! Welcome to instaBot!'
+        print 'Here are your menu options:'
+        print "a.Get your own details"
+        print "b.Get details of a user by username"
+        print "c.Get your own recent post"
+        print "d.Get the recent post of a user by username"
+        print "e.Like the recent post of a user"
+        print "f.Make a comment on the recent post of a user"
+        print "g.Exit"
 
-print get_user_id("vivek_shivam")
-get_user_info("vivek_shivam")
-get_own_post()
-get_user_post("vivek_shivam")
-get_post_id("vivek_shivam")
-like_a_post("vivek_shivam")
-post_a_comment("vivek_shivam")
+        choice = raw_input("Enter you choice: ")
+        if choice == "a":
+            self_info()
+        elif choice == "b":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_info(insta_username)
+        elif choice == "c":
+            get_own_post()
+        elif choice == "d":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_post(insta_username)
+        elif choice == "e":
+           insta_username = raw_input("Enter the username of the user: ")
+           like_a_post(insta_username)
+        elif choice == "f":
+           insta_username = raw_input("Enter the username of the user: ")
+           post_a_comment(insta_username)
+        elif choice == "g":
+            exit()
+        else:
+            print "wrong choice"
+
+start_bot()
